f7bebb14061ce5015419cb75558b882d6d5aafd4
Mzwa <m.shawnlouw@gmail.com>
2019-12-03 12:25:23 +0200

    passing tests for db and app functionality

--- registration.js
+++ registration.js
@@ -1,126 +1,80 @@
 module.exports = function RegistrationOpp(pool) {
 
-  // var regex = /^[A-Za-z]{2}\s[-0-9\s]{3}\s[0-9]{3}$/.toUpperCase;
-  // let numberPlate;
   let vehicle;
-  // let plate;
-
   let valid_codes = ['all', 'CA', 'CY', 'CJ']
 
   async function addReg(regNumber) {
 
     regNumber = regNumber.toUpperCase();
 
-    var townCode =  regNumber.substring(0, 2).trim()
+    var townCode = regNumber.substring(0, 2).trim()
 
-    if(regNumber === undefined || regNumber === '' || !valid_codes.includes(townCode)){
-      return fasle
+    if (regNumber === undefined || regNumber === '' || !valid_codes.includes(townCode)) {
+      return false
     }
 
-     vehicle = await pool.query(
-        "SELECT * FROM reg_plates WHERE reg_number = $1",
-        [regNumber]
+    vehicle = await pool.query(
+      "SELECT * FROM reg_plates WHERE reg_number = $1",
+      [regNumber]
+    );
+
+    if (vehicle.rowCount === 0) {
+
+      let townID = await pool.query(
+        "SELECT id FROM towns WHERE reg_code = $1",
+        [townCode]
       );
 
-      if(vehicle.rowCount === 0){
-
-        let townID = await pool.query(
-          "SELECT id FROM towns WHERE reg_code = $1",
-          [townCode]
-        );
-
-            await pool.query(
-          "INSERT INTO reg_plates (reg_number, town_id) VALUES ($1,$2)",
-          [regNumber, townID.rows[0].id]
-        );
-        return true;
-      }
-
-    // plate = regNumber;
-
-    // vehicle = await pool.query(
-    //   "SELECT * FROM reg_plates WHERE reg_number = $1",
-    //   [plate]
-    // );
-  
-    // let allValidTowns = await pool.query("SELECT * FROM towns");
-    //   console.log(vehicle.rows.length);
-
-    // let validTowns = allValidTowns.rows;
-
-    // for (let index = 0; index < validTowns.length; index++) {
-    //   let townCode = validTowns[index].reg_code;
-    //    let townId = validTowns[index].id;
-    //   if (plate.startsWith(townCode)) {
-    //     await pool.query(
-    //       "INSERT INTO reg_plates (reg_number, town_id) VALUES ($1,$2)",
-    //       [plate, townId]
-    //     );
-    //   }
-    // }
+      await pool.query(
+        "INSERT INTO reg_plates (reg_number, town_id) VALUES ($1,$2)",
+        [regNumber, townID.rows[0].id]
+      );
+      return true;
+    }
   }
-
-  // async function regDuplicate(){
-  //   vehicle = await pool.query(
-  //     "SELECT * FROM reg_plates WHERE reg_number = $1",
-  //     [plate]
-  //   );
-  //   return vehicle.rows.length
-  // }
 
   async function getRegNumbers() {
     var registrationPlates = await pool.query(
       "SELECT * FROM reg_plates"
-      );
+    );
     return registrationPlates.rows;
   }
 
-  // async function getTowns() {
-  //   var towns = await pool.query("SELECT * FROM towns");
-  //   return towns.rows;
-  // }
-  
-  // async function townCodes(town, code) {
-  //   let getCode = await pool.query(
-  //     "INSERT INTO towns (town, reg_code) VALUES ($1,$2)",
-  //     [town, code]
-  //   );
-  //   return getCode;
-  // }
+  async function filter(tag) {
 
-   async function filter(tag) {
+    // if (!valid_codes.includes(tag)) {
+    //   return false;
+    // }
 
-    if(!valid_codes.includes(tag)){
-      return false;
-    }
+    // let filteredRegNumbers = await pool.query(
+    //   "SELECT reg_number, town_id FROM reg_plates"
+    // );
 
-    let filteredRegNumbers = await pool.query(
-      "SELECT reg_number, town_id FROM reg_plates"
-    );
+    // if (tag !== 'all') {
+
+    //   let townFiltered = await pool.query(
+    //     "SELECT id FROM towns WHERE reg_code = $1",
+    //     [tag]
+    //   );
+
+    //   return filteredRegNumbers.rows.filter(reg => reg.towns == townFiltered.rows[0].id);
+    // } else {
+    //   return filteredRegNumbers.rows
+    // }
+
+    //old filter 
+    let regNumbers = await getRegNumbers();
+     if(tag === 'all'){
+       return regNumbers;
+     }
+
+    let filteredRegNumbers = regNumbers.filter(regNumber => {
+      return regNumber.reg_number.startsWith(tag);
+    })
+    console.log(filteredRegNumbers);
     
-    if(tag !== 'all'){
-
-      let townFiltered = await pool.query(
-        "SELECT id FROM towns WHERE reg_code = $1",
-        [tag]
-      );
-
-      return filteredRegNumbers.rows.filter(reg => reg.towns == townFiltered.rows[0].id);
-    }
-    else{
-      return filteredRegNumbers.rows
-    }
-    // let regNumbers = await getRegNumbers();
-    //  if(tag === 'all'){
-    //    return regNumbers;
-    //  }
-     
-    // let filteredRegNumbers = regNumbers.filter(regNumber => {
-    //   return regNumber.reg_number.startsWith(tag);
-    // })
-
-    // return filteredRegNumbers;
-  } 
+    return filteredRegNumbers;
+  }
 
   async function townSelected(tag) {
 
@@ -130,23 +84,14 @@ module.exports = function RegistrationOpp(pool) {
 
     for (let index = 0; index < storedTowns.rowCount; index++) {
       let element = storedTowns.rows[index];
-      if(element.startsWith === tag){
+      if (element.startsWith === tag) {
         element.selected = true;
       }
     }
     return storedTowns.rows;
-    // var car = regex.test(code);
-    // if (car === true) {
-    //   numberPlate = code.toUpperCase();
-    //   return numberPlate;
-    // } else {
-    //   numberPlate = null;
-    // }
-    // console.log("plate:", numberPlate);
-    // return numberPlate;
   }
 
-  async function clearReg(){
+  async function clearReg() {
     let regDelete = await pool.query('DELETE FROM reg_plates');
     return regDelete.rows;
   }
@@ -155,14 +100,8 @@ module.exports = function RegistrationOpp(pool) {
   return {
     addReg,
     getRegNumbers,
-    // regDuplicate,
-    // townCodes,
     filter,
-    // regCheck,
     townSelected,
-    // getTowns,
     clearReg
   };
 };
-
-
--- test/test.js
+++ test/test.js
@@ -19,111 +19,174 @@ const connectionString =
 
 
 
-describe('Registration testing + database tests', function () {
+describe('Registration testing adding reg for each town and duplicates', function () {
 
     beforeEach(async function(){
         // clean the tables before each test run
         // await pool.query("delete from towns;");
         await pool.query("delete from reg_plates;");
+        await pool.query('insert into towns (town, reg_code) values($1,$2)',['Cape Town', 'CA'])
+        await pool.query('insert into towns (town, reg_code) values($1,$2)',['Bellville', 'CY'])
+        await pool.query('insert into towns (town, reg_code) values($1,$2)',['Paarl', 'CJ'])
     });
 
 
-    it('should display valid reg numbers containing CA for the town Cape Town', function () {
+    it('should display valid reg numbers containing CA for the town Cape Town', async function () {
 
         let input = RegistrationOpp(pool);
         await input.addReg("CA 125258")
        await  input.addReg("CA 987 456")
 
         let regCA =  await input.getRegNumbers()
-        assert.deepEqual(regCA, ["CA 125258", "CA 987 456"])
+        var results = regCA.map(function(registration){
+            return registration.reg_number
+        });
+        // console.log("results:", results)
+        assert.deepEqual(results, ["CA 125258", "CA 987 456"])
     })
 
-    // it('should display valid reg numbers containg CY for the town Bellville', function () {
-    //     await pool.query("delete from towns;");
-    //     await pool.query("delete from reg_plates;");
+    it('should display valid reg numbers containg CY for the town Bellville', async function () {
 
-    //     let input = RegistrationOpp(pool);
-    //     await input.addReg("CY 458")
-    //     await input.addReg("CY 876456")
-    //     await input.addReg("CY 459 008")
-    //     assert.deepEqual(input.getRegNumbers(), ["CY 458", "CY 876456", "CY 459 008"])
-    // })
+        let input = RegistrationOpp(pool);
+        await input.addReg("CY 458")
+        await input.addReg("CY 876456")
+        await input.addReg("CY 459 008")
 
-    // it('should display valid reg numbers containing CJ for the town Paarl', function () {
-    //     await pool.query("delete from towns;");
-    //     await pool.query("delete from reg_plates;");
+        let regCY = await input.getRegNumbers()
+        var results = regCY.map(function(registration){
+            return registration.reg_number
+        });
 
-    //     let input = RegistrationOpp(pool);
-    //     input.addReg("CJ 585 458")
-    //     input.addReg("CJ 786 678")
-    //     assert.deepEqual(input.getRegNumbers(), ["CJ 585 458", "CJ 786 678"])
-    // })
+        assert.deepEqual(results, ["CY 458", "CY 876456", "CY 459 008"])
+    })
 
-    // it('should display duplicated reg numbers as one entry', function () {
-    //     await pool.query("delete from towns;");
-    //     await pool.query("delete from reg_plates;");
+    it('should display valid reg numbers containing CJ for the town Paarl', async function () {
 
-    //     let input = RegistrationOpp(pool);
-    //     input.addReg("CA 125258")
-    //     input.addReg("CA 125258")
-    //     input.addReg("CJ 585 458")
-    //     input.addReg("CJ 585 458")
-    //     input.addReg("CY 458")
-    //     input.addReg("CY 458")
-    //     assert.deepEqual(input.getRegNumbers(), ["CA 125258", "CJ 585 458", "CY 458"])
-    // })
+        let input = RegistrationOpp(pool);
+       await input.addReg("CJ 585 458")
+       await input.addReg("CJ 786 678")
 
-    // it('should display reg numbers containing CA only, if filtered for Cape Town', function () {
-    //     await pool.query("delete from towns;");
-    //     await pool.query("delete from reg_plates;");
+       let regCJ = await input.getRegNumbers()
+       var results = regCJ.map(function(registration){
+           return registration.reg_number
+       });
 
-    //     let input = RegistrationOpp(pool);
-    //     input.addReg("CA 567 890")
-    //     input.addReg("CJ 246 894")
-    //     input.addReg("CY 15875")
-    //     input.addReg("CA 8775")
-    //     assert.deepEqual(input.filter("CA"), ["CA 567 890", "CA 8775"])
-    // })
+        assert.deepEqual(results, ["CJ 585 458", "CJ 786 678"])
+    })
 
-    // it('should display reg numbers containing CY only, if filtered for Bellville', function () {
-    //     await pool.query("delete from towns;");
-    //     await pool.query("delete from reg_plates;");
+    it('should display duplicated reg numbers as one entry', async function () {
 
-    //     let input = RegistrationOpp(pool);
-    //     input.addReg("CA 567 890")
-    //     input.addReg("CY 246 894")
-    //     input.addReg("CY 15875")
-    //     input.addReg("CA 8775")
-    //     assert.deepEqual(input.filter("CY"), ["CY 246 894", "CY 15875"])
-    // })
+        let input = RegistrationOpp(pool);
+        await input.addReg("CA 125258")
+        await input.addReg("CA 125258")
+        await input.addReg("CJ 585 458")
+        await input.addReg("CJ 585 458")
+        await input.addReg("CY 458")
+        await input.addReg("CY 458")
 
-    // it('should display reg numbers containing CJ only, if filtered for Paarl', function () {
-    //     await pool.query("delete from towns;");
-    //     await pool.query("delete from reg_plates;");
+        let regs = await input.getRegNumbers()
+        var results = regs.map(function(registration){
+            return registration.reg_number
+        });
 
-    //     let input = RegistrationOpp(pool);
-    //     input.addReg("CJ 567 890")
-    //     input.addReg("CJ 246 894")
-    //     input.addReg("CY 15875")
-    //     input.addReg("CJ 8775")
-    //     input.addReg("CA 567 890")
-    //     input.addReg("CL 246 894")
-    //     input.addReg("CJ 15875")
-    //     input.addReg("CA 8775")
-    //     assert.deepEqual(input.filter("CJ"), ["CJ 567 890", "CJ 246 894", "CJ 8775", "CJ 15875"])
-    // })
+        assert.deepEqual(results, ["CA 125258", "CJ 585 458", "CY 458"])
+    });
+    
 
-    // it('should display success message if reg number added is valid and town is recognised', function () {
-    //     await pool.query("delete from towns;");
-    //     await pool.query("delete from reg_plates;");
+    it('should display reg numbers containing CA only, if filtered for Cape Town', async function () {
+        
+        let input = RegistrationOpp(pool);
+        await input.addReg("CA 567 890")
+        await input.addReg("CJ 246 894")
+        await input.addReg("CY 15875")
+        await input.addReg("CA 8775")
 
-    //     let input = RegistrationOpp(pool);
-    //     input.addReg("CA 567 890")
-    //     input.addReg("CJ 246 894")
-    //     input.addReg("CY 15875")
-    //     input.addReg("CA 8775")
-    //     assert.deepEqual(input.regMsg(), "Registration number added successfully!")
-    // })
+        var list=[]
+         let filterCA = await input.filter("CA")        
+        for(var i = 0;i<filterCA.length;i++){
+            var check = filterCA[i]
+        
+        list.push(check.reg_number)
+        }
+
+        assert.deepEqual(list, ['CA 567 890',"CA 8775"])
+    });
+
+    it('should display reg numbers containing CY only, if filtered for Bellville', async function () {
+
+        let input = RegistrationOpp(pool);
+        await input.addReg("CA 567 890")
+        await input.addReg("CY 246 894")
+        await input.addReg("CY 15875")
+        await input.addReg("CA 8775")
+
+        var list = []
+        let filterCY = await input.filter("CY")        
+        for(var i = 0;i<filterCY.length;i++){
+            var check = filterCY[i]
+        
+        list.push(check.reg_number)
+        }
+        assert.deepEqual(list, ["CY 246 894", "CY 15875"])
+    });
+
+    it('should display reg numbers containing CJ only, if filtered for Paarl', async function () {
+
+        let input = RegistrationOpp(pool);
+        await input.addReg("CJ 567 890")
+        await input.addReg("CJ 246 894")
+        await input.addReg("CY 15875")
+        await input.addReg("CJ 8775")
+        await input.addReg("CA 567 890")
+        await input.addReg("CJ 15875")
+        await input.addReg("CA 8775")
+
+        var list = []
+        let filterCJ = await input.filter("CJ")        
+        for(var i = 0;i<filterCJ.length;i++){
+            var check = filterCJ[i]
+        
+        list.push(check.reg_number)
+        }
+        assert.deepEqual(list, ["CJ 567 890", "CJ 246 894", "CJ 8775", "CJ 15875"])
+    })
+
+    it('should return all reg numbers, if filtered for All Towns', async function () {
+
+        let input = RegistrationOpp(pool);
+        await input.addReg("CJ 246 894")
+        await input.addReg("CY 15875")
+        await input.addReg("CJ 8775")
+        await input.addReg("CA 567 890")
+     
+        var list = []
+        let allRegs = await input.filter("all")
+        for(var i = 0;i<allRegs.length;i++){
+            var check = allRegs[i]
+
+            list.push(check.reg_number)
+        }
+
+        assert.deepEqual(list, ["CJ 246 894", "CY 15875", "CJ 8775", "CA 567 890"])
+    });
+
+    it('should clear all data on screen and database', async function(){
+
+        let input = RegistrationOpp(pool);
+        
+        await input.addReg("CJ 567 890")
+        await input.addReg("CJ 246 894")
+        await input.addReg("CY 15875")
+        await input.addReg("CJ 8775")
+        await input.addReg("CA 567 890")
+        await input.addReg("CJ 15875")
+        await input.addReg("CA 8775")
+
+        let clearReg = await pool.query('delete from reg_plates')
+        await pool.query('delete from towns');
+
+        assert.deepEqual(clearReg.rows, []);
+    });
 
     after(function(){
         pool.end();
